fdd22aad1791284024554d27875556b02916b83a
Lenard Frommelt <lenny09918050@googlemail.com>
2019-12-19 09:00:39 +0100

    fix

--- individualize.js
+++ individualize.js
@@ -2,5 +2,5 @@ const exec = require("child_process").exec
 const pathModule = require("path")
 
 var toolsetDir = pathModule.resolve(process.cwd(), "toolset")
-var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForPWA.pl")
+var scriptPath = pathModule.resolve(toolsetDir, "prepareThingyForPwa.pl")
 exec(scriptPath, {cwd:toolsetDir}, (err, out, stderr) => console.log(out + "\nindividualized pwa!"))
